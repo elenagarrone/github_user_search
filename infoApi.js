92df2e06fd8c19f24e32dfb54d6639a1897dad7b
Elena <elena.15@hotmail.it>
2014-12-09 14:01:13 +0000

    styled blocks with user info

--- infoApi.js
+++ infoApi.js
@@ -2,8 +2,7 @@ $('#fetch_github_profile').on('click', function() {
   var userName = $('#profile_name').val();
   var source = $("#profileTemplate").html();
   var template = Handlebars.compile(source);
-  var profiles = $('#profiles').css('background-color', 'white');
   $.get('https://api.github.com/users/' + userName + '?client_id=' + id + '&client_secret=' + secret, function(data) {
-    profiles.append(template(data));
+    $('#profiles').append(template(data));
   });
 });
