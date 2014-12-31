40bd31270e538b2c5a16f2635b5d4f6a48a72778
Elena <elena.15@hotmail.it>
2014-12-31 16:07:24 +0000

    fixed indentation

--- public/infoApi.js
+++ public/infoApi.js
@@ -2,7 +2,8 @@ $('#fetch_github_profile').on('click', function() {
   var userName = $('#profile_name').val();
   var source = $("#profileTemplate").html();
   var template = Handlebars.compile(source);
-  $.get('https://api.github.com/users/' + userName + '?client_id=' + "#{ENV['GUS_ID']}" + '&client_secret=' + "#{ENV['GUS_SECRET']}", function(data) {
+  $.get('https://api.github.com/users/' + userName + '?client_id=' + "#{ ENV['GUS_ID'] }" + '&client_secret=' + "#{ ENV['GUS_SECRET'] }", function(data) {
     $('#profiles').append(template(data));
   });
+  $('#profile_name').val('');
 });
